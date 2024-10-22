5a7c337609125e55a0ddb3e77ddd144fa601a7c1
Stickfail <150457966+Stickfail@users.noreply.github.com>
2024-10-22 10:52:50 -0400

    finally

--- GameFiles/FHJSON.js
+++ GameFiles/FHJSON.js
@@ -4,7 +4,7 @@ const imgs = {
     "Atreus1" : document.createElement("img"),
     "Atreus2" : document.createElement("img")
 }
-imgs.Atreus1.src = "assets/AtreusSheet.png"
+imgs.Atreus1.src = "assets/AtreusIdle.png"
 imgs.Atreus2.src = "assets/AtreusSheet (1).png"
 
 
