ef035bc1c8516ae7c8d37a5757084e37006e2eb0
Stickfail <150457966+Stickfail@users.noreply.github.com>
2024-10-23 14:36:16 -0400

    alkskdkksajadal

--- GameFiles/FHJSON.js
+++ GameFiles/FHJSON.js
@@ -11,6 +11,20 @@ imgs.Atreus2.src = "assets/AtreusSheet (1).png"
 const startingCharacters = {
     1:{
         name:"Player"
+    },
+    2:{
+        name:"Atreus",
+        class:"Mage",
+        lvl:1,
+        maxhp:0,
+        maxmp:0,
+        exp:0,
+        spd:0.2,
+        atk:0,
+        def:0,
+        skills:[],
+        effects:[],
+        equipped:[]
     }
 }
 const currentCharacters = {
@@ -22,5 +36,22 @@ const enemies = {
 }
 
 const items = {
-
+    1:{
+        name:"Lesser Healing Potion"
+    },
+    2:{
+        name:"Healing Potion"
+    },
+    3:{
+        name:"Greater Healing Potion"
+    },
+    4:{
+        name:"Lesser Mana Potion"
+    },
+    5:{
+        name:"Mana Potion"
+    },
+    6:{
+        name:"Greater Mana Potion"
+    }
 }
